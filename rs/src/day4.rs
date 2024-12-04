53977edeadd2df1074d472bcb7022f77b5011ce0
Daniel Lin <ephemient@gmail.com>
2024-12-04 10:43:11 -0500

    Extract constants

--- rs/src/day4.rs
+++ rs/src/day4.rs
@@ -1,29 +1,34 @@
+use array_util::try_from_fn;
 use itertools::izip;
 
 pub fn part1(data: &str) -> usize {
+    const XMAS: [[u8; 4]; 2] = [*b"XMAS", *b"SAMX"];
+
     let lines = &data.lines().collect::<Vec<_>>()[..];
     lines
         .iter()
         .enumerate()
         .flat_map(|(y, line)| {
             (0..line.len()).flat_map(move |x| {
-                (-1..=1).flat_map(move |dy| {
-                    (-1..=1).filter(move |dx| {
-                        "XMAS".bytes().enumerate().all(|(i, b)| {
+                [(1, 0), (1, 1), (0, 1), (-1, 1)]
+                    .iter()
+                    .filter_map(move |(dx, dy)| {
+                        try_from_fn(|i| {
                             y.checked_add_signed(i as isize * dy)
                                 .and_then(|y| lines.get(y))
                                 .zip(x.checked_add_signed(i as isize * dx))
-                                .and_then(|(line, x)| line.as_bytes().get(x))
-                                == Some(&b)
+                                .and_then(|(line, x)| line.as_bytes().get(x).copied())
                         })
                     })
-                })
             })
         })
+        .filter(|s| XMAS.contains(s))
         .count()
 }
 
 pub fn part2(data: &str) -> usize {
+    const XMAS: [[u8; 5]; 4] = [*b"MMASS", *b"MSAMS", *b"SMASM", *b"SSAMM"];
+
     let lines = data.lines().collect::<Vec<_>>();
     izip!(&lines[..], &lines[1..], &lines[2..])
         .flat_map(|(above, line, below)| {
@@ -34,12 +39,8 @@ pub fn part2(data: &str) -> usize {
                 below.bytes(),
                 below.bytes().skip(2)
             )
-            .filter(|&(nw, ne, b, sw, se)| {
-                b == b'A'
-                    && (nw == b'M' && se == b'S' || se == b'M' && nw == b'S')
-                    && (ne == b'M' && sw == b'S' || sw == b'M' && ne == b'S')
-            })
         })
+        .filter(|&(nw, ne, b, sw, se)| XMAS.contains(&[nw, ne, b, sw, se]))
         .count()
 }
 

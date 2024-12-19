f908a449c7f379ed1386c19e39446c4b27a1e26e
Daniel Lin <ephemient@gmail.com>
2024-12-19 01:17:02 -0500

    Unnecessary sort

--- hs/src/Day19.hs
+++ hs/src/Day19.hs
@@ -6,24 +6,22 @@
 module Day19 (solve) where
 
 import Control.Arrow ((***))
+import Control.Monad (forM_, when)
 import Control.Monad.ST (runST)
 import Data.Foldable (foldMap')
-import Data.List (sort)
 import Data.Monoid (Sum (Sum, getSum))
 import Data.Text (Text)
 import Data.Text qualified as T (isPrefixOf, length, lines, null, splitOn, tails)
-import Data.Vector.Unboxed.Mutable qualified as MV (new, read, write)
+import Data.Vector.Unboxed.Mutable qualified as MV (modify, new, read, write)
 
 count :: [Text] -> Text -> (Sum Int, Sum Int)
 count keys target = runST $ do
   acc <- MV.new $ T.length target + 1
   MV.write acc 0 1
-  mapM_ (\(i, j) -> (+) <$> MV.read acc i <*> MV.read acc j >>= MV.write acc i) . sort $
-    [ (offset + T.length key, offset)
-    | key <- keys,
-      (offset, target') <- zip [0 ..] $ T.tails target,
-      key `T.isPrefixOf` target'
-    ]
+  forM_ (zip [0 ..] $ T.tails target) $ \(i, target') -> do
+    n <- MV.read acc i
+    forM_ keys $ \key ->
+      when (key `T.isPrefixOf` target') $ MV.modify acc (+ n) $ i + T.length key
   n <- MV.read acc $ T.length target
   pure (Sum $ if n == 0 then 0 else 1, Sum n)
 

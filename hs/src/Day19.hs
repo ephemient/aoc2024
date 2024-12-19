1092f683e7874f2f80ac93ecd6cef5f17f75250d
Daniel Lin <ephemient@gmail.com>
2024-12-19 01:33:34 -0500

    Tying the knot to avoid mutability

--- hs/src/Day19.hs
+++ hs/src/Day19.hs
@@ -6,27 +6,21 @@
 module Day19 (solve) where
 
 import Control.Arrow ((***))
-import Control.Monad (forM_, when)
-import Control.Monad.ST (runST)
 import Data.Foldable (foldMap')
 import Data.Monoid (Sum (Sum, getSum))
 import Data.Text (Text)
-import Data.Text qualified as T (isPrefixOf, length, lines, null, splitOn, tails)
-import Data.Vector.Unboxed.Mutable qualified as MV (modify, new, read, write)
+import Data.Text qualified as T (isSuffixOf, length, lines, null, splitOn, take)
+import Data.Vector qualified as V (generate, (!))
 
-count :: [Text] -> Text -> (Sum Int, Sum Int)
-count keys target = runST $ do
-  acc <- MV.new $ T.length target + 1
-  MV.write acc 0 1
-  forM_ (zip [0 ..] $ T.tails target) $ \(i, target') -> do
-    n <- MV.read acc i
-    forM_ keys $ \key ->
-      when (key `T.isPrefixOf` target') $ MV.modify acc (+ n) $ i + T.length key
-  n <- MV.read acc $ T.length target
-  pure (Sum $ if n == 0 then 0 else 1, Sum n)
+count :: [Text] -> Text -> Int
+count keys target = getCount $ T.length target
+  where
+    counts = V.generate (T.length target) getCount
+    getCount 0 = 1
+    getCount i = sum [counts V.! (i - T.length key) | key <- keys, key `T.isSuffixOf` T.take i target]
 
 solve :: Text -> (Int, Int)
 solve input
   | keys : rest <- T.lines input =
-      (getSum *** getSum) $ foldMap' (count $ T.splitOn ", " keys) $ filter (not . T.null) rest
+      (getSum *** getSum) . foldMap' ((Sum 1,) . Sum) . filter (> 0) $ count (T.splitOn ", " keys) <$> filter (not . T.null) rest
   | otherwise = (0, 0)

0dc259131ba20bda21051fff6c61ff5f45cffd4b
Daniel Lin <ephemient@gmail.com>
2024-12-23 02:07:22 -0500

    Day 23: LAN Party

---
+++ rs/src/day23.rs
@@ -0,0 +1,110 @@
+use std::collections::{BTreeMap, BTreeSet};
+
+use itertools::Itertools;
+
+fn parse(data: &str) -> BTreeMap<&str, BTreeSet<&str>> {
+    let mut graph = BTreeMap::<_, BTreeSet<_>>::new();
+    for line in data.lines() {
+        let Some((a, b)) = line.split_once('-') else {
+            continue;
+        };
+        graph.entry(a.min(b)).or_default().insert(a.max(b));
+        graph.entry(a.max(b)).or_default();
+    }
+    graph
+}
+
+pub fn part1(data: &str) -> usize {
+    let graph = parse(data);
+    graph
+        .iter()
+        .flat_map(|(a, bs)| {
+            let graph = &graph;
+            bs.iter().flat_map(move |b| {
+                graph[b].intersection(bs).filter(|c| {
+                    bs.contains(*c)
+                        && (a.starts_with('t') || b.starts_with('t') || c.starts_with('t'))
+                })
+            })
+        })
+        .count()
+}
+
+pub fn part2(data: &str) -> String {
+    let graph = parse(data);
+    fn max_complete<'a>(
+        graph: &'a BTreeMap<&'a str, BTreeSet<&'a str>>,
+        used: BTreeSet<&'a str>,
+        remaining: BTreeSet<&'a str>,
+    ) -> BTreeSet<&'a str> {
+        remaining
+            .iter()
+            .map(|a| {
+                let mut used = used.clone();
+                used.insert(*a);
+                max_complete(
+                    graph,
+                    used,
+                    remaining.intersection(&graph[a]).copied().collect(),
+                )
+            })
+            .max_by_key(BTreeSet::len)
+            .unwrap_or(used)
+    }
+    max_complete(&graph, BTreeSet::new(), graph.keys().copied().collect())
+        .iter()
+        .sorted()
+        .join(",")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+    use pretty_assertions::assert_eq;
+
+    static EXAMPLE: &str = indoc! {"
+        kh-tc
+        qp-kh
+        de-cg
+        ka-co
+        yn-aq
+        qp-ub
+        cg-tb
+        vc-aq
+        tb-ka
+        wh-tc
+        yn-cg
+        kh-ub
+        ta-co
+        de-co
+        tc-td
+        tb-wq
+        wh-td
+        ta-ka
+        td-qp
+        aq-cg
+        wq-ub
+        ub-vc
+        de-ta
+        wq-aq
+        wq-vc
+        wh-yn
+        ka-de
+        kh-ta
+        co-tc
+        wh-qp
+        tb-vc
+        td-yn
+    "};
+
+    #[test]
+    fn part1_examples() {
+        assert_eq!(7, part1(EXAMPLE));
+    }
+
+    #[test]
+    fn part2_examples() {
+        assert_eq!("co,de,ka,ta".to_string(), part2(EXAMPLE));
+    }
+}
--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,7 +1,7 @@
 use anyhow::anyhow;
 use aoc2024::{
     day1, day10, day11, day12, day13, day14, day15, day16, day17, day18, day19, day2, day20, day21,
-    day22, day3, day4, day5, day6, day7, day8, day9,
+    day22, day23, day3, day4, day5, day6, day7, day8, day9,
 };
 use std::collections::HashSet;
 use std::env;
@@ -194,6 +194,14 @@ fn main() -> anyhow::Result<()> {
         let data = get_day_input(22)?;
         println!("{:?}", day22::part1(&data));
         println!("{:?}", day22::part2(&data));
+        println!();
+    }
+
+    if args.is_empty() || args.contains("23") {
+        println!("Day 23");
+        let data = get_day_input(23)?;
+        println!("{:?}", day23::part1(&data));
+        println!("{}", day23::part2(&data));
         println!();
     }
 

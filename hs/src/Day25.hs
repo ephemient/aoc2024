8a2558e7afbeadbe3810684c2d1a4d652ad148c7
Daniel Lin <ephemient@gmail.com>
2024-12-25 00:31:38 -0500

    Day 25: Make use of List monad

--- hs/src/Day25.hs
+++ hs/src/Day25.hs
@@ -6,6 +6,7 @@
 module Day25 (part1) where
 
 import Control.Arrow (Arrow ((&&&)))
+import Data.Function (on)
 import Data.List (partition)
 import Data.List.NonEmpty (nonEmpty)
 import Data.List.NonEmpty qualified as NonEmpty (head)
@@ -14,13 +15,7 @@ import Data.Text (Text)
 import Data.Text qualified as T (group, head, length, lines, splitOn, transpose)
 
 part1 :: Text -> Int
-part1 input =
-  length
-    [ ()
-    | key <- keys,
-      lock <- locks,
-      and [k >= l | ((_, k), (_, l)) <- zip key lock]
-    ]
+part1 input = length . filter and $ zipWith ((>=) `on` snd) <$> keys <*> locks
   where
     stanzas = mapMaybe (fmap ((T.head &&& T.length) . NonEmpty.head) . nonEmpty . T.group) . T.transpose . T.lines <$> T.splitOn "\n\n" input
     (keys, locks) = partition (maybe False ((== '.') . fst . NonEmpty.head) . nonEmpty) stanzas

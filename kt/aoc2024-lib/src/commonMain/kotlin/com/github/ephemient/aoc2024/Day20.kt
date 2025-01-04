0ce46c8df078c0fcbb908183e72e902d303e39c5
Daniel Lin <ephemient@gmail.com>
2025-01-03 19:47:40 -0500

    Day 20: Parallelize

--- kt/aoc2024-lib/src/commonMain/kotlin/com/github/ephemient/aoc2024/Day20.kt
+++ kt/aoc2024-lib/src/commonMain/kotlin/com/github/ephemient/aoc2024/Day20.kt
@@ -1,6 +1,10 @@
 package com.github.ephemient.aoc2024
 
 import kotlin.math.abs
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.fold
 
 class Day20(input: String) {
     private val path = buildSet {
@@ -18,13 +22,13 @@ class Day20(input: String) {
         } while (lines[position.first][position.second] != 'E')
     }.withIndex().sortedWith(compareBy(comparator) { it.value })
 
-    fun part1(time: Int = 100) = solve(2, time)
+    suspend fun part1(time: Int = 100) = solve(2, time)
 
-    fun part2(time: Int = 100) = solve(20, time)
+    suspend fun part2(time: Int = 100) = solve(20, time)
 
-    private fun solve(cheats: Int, time: Int): Int {
-        var count = 0
-        for (i in path.indices) {
+    private suspend fun solve(cheats: Int, time: Int) = path.indices.asFlow().flatMapMerge { i ->
+        flow {
+            var count = 0
             val (t1, pos1) = path[i]
             val limit = pos1.first + cheats to pos1.second
             for (j in i + 1..<path.size) {
@@ -33,9 +37,9 @@ class Day20(input: String) {
                 val distance = abs(pos2.first - pos1.first) + abs(pos2.second - pos1.second)
                 if (distance <= cheats && distance + time <= abs(t2 - t1)) count++
             }
+            emit(count)
         }
-        return count
-    }
+    }.fold(0, Int::plus)
 
     companion object {
         private val comparator = compareBy(IntPair::first, IntPair::second)

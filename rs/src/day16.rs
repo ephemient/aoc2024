a5bd30357254676942a904bf8f92555421a81882
Daniel Lin <ephemient@gmail.com>
2025-01-05 08:17:28 -0500

    Day 16: Build reverse links to enable fast traversal of all paths

--- rs/src/day16.rs
+++ rs/src/day16.rs
@@ -1,10 +1,10 @@
-use if_chain::if_chain;
 use std::cmp::Reverse;
-use std::collections::{BTreeMap, BTreeSet, BinaryHeap};
+use std::collections::{btree_map, BTreeMap, BTreeSet, BinaryHeap};
 
-type YX = (usize, usize);
+type Point = (usize, usize);
+type Direction = (isize, isize);
 
-fn parse(data: &str) -> Option<(BTreeSet<YX>, YX, YX)> {
+fn parse(data: &str) -> Option<(BTreeSet<Point>, Point, Point)> {
     let mut maze = BTreeSet::new();
     let mut start = None;
     let mut end = None;
@@ -31,73 +31,101 @@ fn parse(data: &str) -> Option<(BTreeSet<YX>, YX, YX)> {
     Some((maze, start?, end?))
 }
 
-pub fn part1(data: &str) -> Option<usize> {
-    let (maze, start, end) = parse(data)?;
-    let mut queue: BinaryHeap<_> = [(Reverse(0), start, (0, 1))].into();
-    let mut visited = BTreeSet::new();
-    while let Some((Reverse(score), (y, x), (dy, dx))) = queue.pop() {
-        if !visited.insert(((y, x), (dy, dx))) {
-            continue;
-        }
-        if (y, x) == end {
-            return Some(score);
-        }
-        for (score, (dy, dx)) in [
-            (score + 1, (dy, dx)),
-            (score + 1001, (-dx, dy)),
-            (score + 1001, (dx, -dy)),
-        ] {
-            if_chain! {
-                if let Some(y) = y.checked_add_signed(dy);
-                if let Some(x) = x.checked_add_signed(dx);
-                if !maze.contains(&(y, x));
-                then {
-                    queue.push((Reverse(score), (y, x), (dy, dx)));
-                }
-            }
+struct Explore<F> {
+    maze: F,
+    queue: BinaryHeap<(Reverse<usize>, (Point, Direction), u8)>,
+    visited: BTreeMap<(Point, Direction), usize>,
+}
+
+impl<F> Explore<F> {
+    fn new(maze: F, start: Point) -> Self {
+        Self {
+            maze,
+            queue: [(Reverse(0), (start, (0, 1)), 0)].into(),
+            visited: BTreeMap::new(),
         }
     }
-    None
+}
+
+impl<F> Iterator for Explore<F>
+where
+    F: Fn(Point) -> bool,
+{
+    type Item = (usize, (Point, Direction), u8);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let (Reverse(score), state @ ((y, x), (dy, dx)), way) = self.queue.pop()?;
+            match self.visited.entry(state) {
+                btree_map::Entry::Vacant(entry) => {
+                    entry.insert(score);
+                }
+                btree_map::Entry::Occupied(entry) => {
+                    let last_score = *entry.get();
+                    assert!(last_score <= score);
+                    if last_score < score {
+                        continue;
+                    }
+                    break Some((score, state, way));
+                }
+            }
+            for (score, (dy, dx), way) in [
+                (score + 1, (dy, dx), 1),
+                (score + 1001, (-dx, dy), 2),
+                (score + 1001, (dx, -dy), 4),
+            ] {
+                let pos = (y.wrapping_add_signed(dy), x.wrapping_add_signed(dx));
+                if !(self.maze)(pos) {
+                    self.queue.push((Reverse(score), (pos, (dy, dx)), way));
+                }
+            }
+            break Some((score, state, way));
+        }
+    }
+}
+
+pub fn part1(data: &str) -> Option<usize> {
+    let (maze, start, end) = parse(data)?;
+    Some(
+        Explore::new(|pos| maze.contains(&pos), start)
+            .find(|(_, (pos, _), _)| *pos == end)?
+            .0,
+    )
 }
 
 pub fn part2(data: &str) -> Option<usize> {
     let (maze, start, end) = parse(data)?;
     let mut best = None;
-    let mut acc = BTreeSet::new();
-    let mut queue: BinaryHeap<(_, _, _, BTreeSet<_>)> =
-        [(Reverse(0), start, (0, 1), [start].into())].into();
-    let mut visited = BTreeMap::new();
-    while let Some((Reverse(score), (y, x), (dy, dx), mut path)) = queue.pop() {
-        if best.filter(|best| *best < score).is_some() {
+    let mut paths = BTreeMap::new();
+    for (score, state @ (pos, _), way) in Explore::new(|pos| maze.contains(&pos), start) {
+        if best.is_some_and(|best| best < score) {
             break;
         }
-        if *visited.entry(((y, x), (dy, dx))).or_insert(score) < score {
-            continue;
-        }
-        if (y, x) == end {
+        paths.entry(state).and_modify(|e| *e |= way).or_insert(way);
+        if pos == end {
             best = Some(score);
-            acc.append(&mut path);
-            continue;
         }
-        for (score, (dy, dx)) in [
-            (score + 1, (dy, dx)),
-            (score + 1001, (-dx, dy)),
-            (score + 1001, (dx, -dy)),
-        ] {
-            if_chain! {
-                if let Some(y) = y.checked_add_signed(dy);
-                if let Some(x) = x.checked_add_signed(dx);
-                if !maze.contains(&(y, x));
-                then {
-                    let mut path = path.clone();
-                    if path.insert((y, x)) {
-                        queue.push((Reverse(score), (y, x), (dy, dx), path));
-                    }
-                }
+    }
+    let mut stack = vec![(end, (0, 1)), (end, (1, 0)), (end, (0, -1)), (end, (-1, 0))];
+    let mut visited = stack.iter().cloned().collect::<BTreeSet<_>>();
+    while let Some(key @ ((y, x), dir @ (dy, dx))) = stack.pop() {
+        let Some(ways) = paths.get(&key) else {
+            continue;
+        };
+        let pos = (y.wrapping_add_signed(-dy), x.wrapping_add_signed(-dx));
+        for (way, dir) in [(1, dir), (2, (dx, -dy)), (4, (-dx, dy))] {
+            if ways & way != 0 && visited.insert((pos, dir)) {
+                stack.push((pos, dir));
             }
         }
     }
-    Some(acc.len())
+    let mut last = None;
+    Some(
+        visited
+            .iter()
+            .filter(|(pos, _)| Some(*pos) != last.replace(*pos))
+            .count(),
+    )
 }
 
 #[cfg(test)]

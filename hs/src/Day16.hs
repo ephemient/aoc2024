c1057c15c30c23bab71a0882967543e0ef5fc27c
Daniel Lin <ephemient@gmail.com>
2025-01-05 08:14:46 -0500

    Day 16: Build reverse links to enable fast traversal of all paths

--- hs/src/Day16.hs
+++ hs/src/Day16.hs
@@ -5,13 +5,14 @@
 -- Description:    <https://adventofcode.com/2024/day/16 Day 16: Reindeer Maze>
 module Day16 (part1, part2) where
 
-import Control.Exception (assert)
-import Data.Bifunctor (second)
+import Data.Bits ((.&.), (.|.))
 import Data.Heap (FstMinPolicy)
 import Data.Heap qualified as Heap (insert, singleton, view)
-import Data.Map qualified as Map (empty, insert, (!?))
+import Data.List (find, mapAccumL)
+import Data.Map qualified as Map (empty, findWithDefault, insert, (!?))
+import Data.Map.Strict qualified as Map (fromListWith)
 import Data.Set (Set)
-import Data.Set qualified as Set (empty, fromList, insert, member, notMember, size)
+import Data.Set qualified as Set (elems, empty, fromList, insert, notMember)
 import Data.Text (Text)
 import Data.Text qualified as T (foldl)
 
@@ -26,33 +27,46 @@ parse input = case T.foldl parse' (0, 0, Set.empty, [], []) input of
     parse' (y, x, maze, start, end) 'E' = (y, x + 1, maze, start, (y, x) : end)
     parse' (y, x, maze, start, end) _ = (y, x + 1, maze, start, end)
 
+walk :: Set (Int, Int) -> (Int, Int) -> [(Int, (((Int, Int), (Int, Int)), Int))]
+walk maze start = walk' Map.empty $ Heap.singleton @FstMinPolicy (0, ((start, (0, 1)), 0))
+  where
+    walk' visited (Heap.view -> Just (state@(score, (pv@((y, x), v@(dy, dx)), _)), queue))
+      | Just score' <- visited Map.!? pv = case compare score score' of
+          LT -> error "invariants violated"
+          EQ -> state : walk' visited queue
+          GT -> walk' visited queue
+      | otherwise = state : walk' (Map.insert pv score visited) queue'
+      where
+        queue' =
+          foldl' (flip Heap.insert) queue . filter ((`Set.notMember` maze) . fst . fst . snd) $
+            [ (score + 1, (((y + dy, x + dx), v), 1)),
+              (score + 1001, (((y - dx, x + dy), (-dx, dy)), 2)),
+              (score + 1001, (((y + dx, x - dy), (dx, -dy)), 4))
+            ]
+    walk' _ _ = []
+
 part1 :: Text -> Maybe Int
 part1 input = do
   (maze, start, end) <- parse input
-  let go visited (Heap.view -> Just ((score, pv@(p@(y, x), v@(dy, dx))), queue'))
-        | p == end = Just score
-        | pv `Set.member` visited = go visited queue'
-        | otherwise =
-            go (Set.insert pv visited) . foldl' (flip Heap.insert) queue' $
-              [(score + 1, (p', v)) | let p' = (y + dy, x + dx), p' `Set.notMember` maze]
-                ++ [(score + 1000, (p, (-dx, dy))), (score + 1000, (p, (dx, -dy)))]
-      go _ _ = Nothing
-  go Set.empty $ Heap.singleton @FstMinPolicy (0 :: Int, (start, (0, 1)))
+  fmap fst . find ((==) end . fst . fst . snd) $ walk maze start
 
 part2 :: Text -> Maybe Int
 part2 input = do
   (maze, start, end) <- parse input
-  best <- part1 input
-  let go acc visited (Heap.view -> Just ((score, (pv@(p@(y, x), v@(dy, dx)), path)), queue))
-        | p == end = assert (score == best) $ go (acc <> Set.fromList (p : path)) visited' queue
-        | Just score' <- visited Map.!? pv, score' < score = go acc visited queue
-        | otherwise =
-            go acc visited' . foldl' (flip Heap.insert) queue . map (second (,path')) . filter ok $
-              [(score + 1, (p', v)) | let p' = (y + dy, x + dx), p' `Set.notMember` maze]
-                ++ [(score + 1000, (p, (-dx, dy))), (score + 1000, (p, (dx, -dy)))]
+  let go1 best ((score, state@((p, _), _)) : rest)
+        | Just best' <- best, best' < score = []
+        | otherwise = state : go1 (if p == end then Just score else best) rest
+      go1 _ _ = []
+      paths = Map.fromListWith (.|.) $ go1 Nothing $ walk maze start
+      go2 visited (pv@((y, x), v@(dy, dx)) : queue) queue' =
+        go2 (foldl' (flip Set.insert) visited next) queue $ next ++ queue'
         where
-          visited' = Map.insert pv score visited
-          path' = p : path
-          ok (score', pv') = score' <= best && maybe True (>= score) (visited Map.!? pv')
-      go acc _ _ = Set.size acc
-  pure $ go Set.empty Map.empty $ Heap.singleton @FstMinPolicy (0 :: Int, ((start, (0, 1)), []))
+          ways = Map.findWithDefault 0 pv paths
+          next =
+            filter (`Set.notMember` visited) . map ((y - dy, x - dx),) $
+              [v | ways .&. 1 /= 0] ++ [(dx, -dy) | ways .&. 2 /= 0] ++ [(-dx, dy) | ways .&. 4 /= 0]
+      go2 visited _ [] = visited
+      go2 visited _ queue = go2 visited (reverse queue) []
+      goals = [(end, v) | v <- [(0, 1), (1, 0), (0, -1), (-1, 0)]]
+      f prev (cur, _) = (Just cur, Just cur /= prev)
+  pure $ length $ filter id $ snd $ mapAccumL f Nothing $ Set.elems $ go2 (Set.fromList goals) goals []

c77669d5a9d5621da07d067a188364b70eced572
Daniel Lin <ephemient@gmail.com>
2025-01-07 01:03:21 -0500

    Day 20: Flatten representation to allow Cython to operate without going through Python

--- py/aoc2024/day20c.py
+++ py/aoc2024/day20c.py
@@ -1,26 +1,22 @@
 # cython: boundscheck=False, wraparound=False, initializedcheck=False, embedsignature=True
 import cython
-from collections.abc import Sequence
 
 
 @cython.ccall
-def solve(
-    path: Sequence[tuple[tuple[int, int], int]], cheats: cython.int, time: cython.int
-) -> cython.int:
-    count: cython.int = len(path)
+@cython.nogil
+def solve(path: cython.int[:], cheats: cython.int, time: cython.int) -> cython.int:
+    count: cython.int = path.shape[0] // 3
     i: cython.int
     result: cython.int = 0
     for i in range(count):
-        y1: cython.int
-        x1: cython.int
-        t1: cython.int
-        (y1, x1), t1 = path[i]
+        y1: cython.int = path[3 * i]
+        x1: cython.int = path[3 * i + 1]
+        t1: cython.int = path[3 * i + 2]
         j: cython.int
         for j in range(i + 1, count):
-            y2: cython.int
-            x2: cython.int
-            t2: cython.int
-            (y2, x2), t2 = path[j]
+            y2: cython.int = path[3 * j]
+            x2: cython.int = path[3 * j + 1]
+            t2: cython.int = path[3 * j + 2]
             if y2 > y1 + cheats or y2 == y1 + cheats and x2 > x1:
                 break
             distance: cython.int = abs(y2 - y1) + abs(x2 - x1)

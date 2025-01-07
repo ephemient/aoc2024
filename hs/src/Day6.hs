ba44346ae21b686afadb1c49c525e2136f2d05da
Daniel Lin <ephemient@gmail.com>
2025-01-07 02:10:20 -0500

    Day 6: Use UArray as bitset instead of Set in part 2

--- hs/src/Day6.hs
+++ hs/src/Day6.hs
@@ -5,57 +5,58 @@ module Day6 (part1, part2) where
 
 import Control.Monad (ap)
 import Control.Parallel.Strategies (parMap, rseq)
+import Data.Array.Unboxed (UArray, inRange, listArray, range, (!), (//))
 import Data.Containers.ListUtils (nubOrd)
-import Data.List.NonEmpty (NonEmpty ((:|)))
+import Data.Ix (Ix)
 import Data.Maybe (catMaybes, isJust)
-import Data.Semigroup (Max (Max), sconcat)
+import Data.Semigroup (Max (Max))
 import Data.Set (Set)
 import Data.Set qualified as Set (empty, insert, member, singleton)
 import Data.Text (Text)
 import Data.Text qualified as T (lines, unpack)
 
-parse :: Text -> ((Int, Int), Set (Int, Int), [((Int, Int), (Int, Int))])
-parse input = ((maxY, maxX), blocks, start)
+parse :: (Enum i, Ix i, Num i, Ord i) => Text -> (((i, i), (i, i)), Set (i, i), [((i, i), (i, i))])
+parse input = (bounds, blocks, start)
   where
     (Max maxY, Max maxX, (blocks, start)) =
-      sconcat $
-        (Max 0, Max 0, mempty)
-          :| [ ( Max y,
-                 Max x,
-                 case char of
-                   '^' -> (mempty, [((y, x), (-1, 0))])
-                   '<' -> (mempty, [((y, x), (0, -1))])
-                   '>' -> (mempty, [((y, x), (0, 1))])
-                   'v' -> (mempty, [((y, x), (1, 0))])
-                   '#' -> (Set.singleton (y, x), mempty)
-                   _ -> mempty
-               )
-             | (y, line) <- zip [0 ..] $ T.lines input,
-               (x, char) <- zip [0 ..] $ T.unpack line
-             ]
+      foldl' (<>) (Max 0, Max 0, mempty) $
+        [ ( Max y,
+            Max x,
+            case char of
+              '^' -> (mempty, [((y, x), (-1, 0))])
+              '<' -> (mempty, [((y, x), (0, -1))])
+              '>' -> (mempty, [((y, x), (0, 1))])
+              'v' -> (mempty, [((y, x), (1, 0))])
+              '#' -> (Set.singleton (y, x), mempty)
+              _ -> mempty
+          )
+        | (y, line) <- zip [0 ..] $ T.lines input,
+          (x, char) <- zip [0 ..] $ T.unpack line
+        ]
+    bounds = ((0, 0), (maxY, maxX))
 
-visited :: (Int, Int) -> Set (Int, Int) -> ((Int, Int), (Int, Int)) -> [((Int, Int), (Int, Int))]
-visited (maxY, maxX) blocks start = catMaybes $ takeWhile isJust $ iterate (>>= step) $ Just start
+visited :: ((Int, Int), (Int, Int)) -> ((Int, Int) -> Bool) -> ((Int, Int), (Int, Int)) -> [((Int, Int), (Int, Int))]
+visited bounds isBlock start = catMaybes $ takeWhile isJust $ iterate (>>= step) $ Just start
   where
     step (pos@(y, x), d@(dy, dx))
-      | y' < 0 || maxY < y' || x' < 0 || maxX < x' = Nothing
-      | (y', x') `Set.member` blocks = Just (pos, (dx, -dy))
-      | otherwise = Just ((y', x'), d)
+      | not $ inRange bounds pos' = Nothing
+      | isBlock pos' = Just (pos, (dx, -dy))
+      | otherwise = Just (pos', d)
       where
-        y' = y + dy
-        x' = x + dx
+        pos' = (y + dy, x + dx)
 
 part1 :: Text -> Int
-part1 input = length $ nubOrd $ map fst $ start >>= visited maxes blocks
+part1 input = length $ nubOrd $ map fst $ start >>= visited bounds (`Set.member` blocks)
   where
-    (maxes, blocks, start) = parse input
+    (bounds, blocks, start) = parse input
 
 part2 :: Text -> Int
 part2 input =
-  length $ filter id $ start >>= (parMap rseq . isLoop) `ap` (nubOrd . map fst . visited maxes blocks)
+  length $ filter id $ start >>= (parMap rseq . isLoop) `ap` (nubOrd . map fst . visited bounds (blocks' !))
   where
-    (maxes, blocks, start) = parse input
-    isLoop start' block = isLoop' 0 Set.empty $ visited maxes (Set.insert block blocks) start'
+    (bounds, blocks, start) = parse input
+    blocks' = listArray @UArray bounds $ (`Set.member` blocks) <$> range bounds
+    isLoop start' block = isLoop' 0 Set.empty $ visited bounds (blocks' // [(block, True)] !) start'
     isLoop' _ _ [] = False
     isLoop' (-1) seen ((_, (dy, _)) : rest) = isLoop' dy seen rest
     isLoop' _ seen ((pos, (-1, _)) : rest) = pos `Set.member` seen || isLoop' (-1) (Set.insert pos seen) rest

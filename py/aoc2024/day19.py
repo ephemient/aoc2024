f4615cc6963c7b995076d5239653aaff45a26811
Daniel Lin <ephemient@gmail.com>
2025-01-06 22:47:12 -0500

    Day 19: Speed up with Cython

--- py/aoc2024/day19.py
+++ py/aoc2024/day19.py
@@ -2,6 +2,8 @@
 Day 19: Linen Layout
 """
 
+from aoc2024.day19c import solve as _solve
+
 SAMPLE_INPUT = """\
 r, wr, b, g, bwu, rb, gb, br
 
@@ -16,15 +18,6 @@ bbrgwb
 """
 
 
-def _count(keys: list[str], target: str) -> int:
-    counts = [1]
-    for i in range(1, len(target) + 1):
-        counts.append(
-            sum(counts[i - len(key)] for key in keys if target[:i].endswith(key))
-        )
-    return counts[-1]
-
-
 def solve(data: str) -> str:
     """
     >>> print(solve(SAMPLE_INPUT))
@@ -33,12 +26,7 @@ def solve(data: str) -> str:
     """
     keys, *targets = data.splitlines()
     keys = keys.split(", ")
-    part1, part2 = 0, 0
-    for target in targets:
-        n = _count(keys, target) if target else None
-        if n:
-            part1 += 1
-            part2 += n
+    part1, part2 = _solve(keys, targets)
     return f"{part1}\n{part2}"
 
 
